7799503e545c9899bf28e73c5c86f1816fa110b1
Fernando souza <fernandofvsouza@icloud.com>
2026-02-12 01:30:51 +0000

    UP

---
+++ JavaTime/JavaTimePratica/Exercicio2.java/src/Exercicio2.java
@@ -0,0 +1,5 @@
+public class Exercicio2 {
+    static void main() {
+        System.out.println("Exercício 2: Somar 30 dias ao dia atual e exibir a data resultante, Subtrair 2 horas do horário atual e exibir o horário resultante, Comparar a data atual com a data resultante da soma de 30 dias e exibir se a data atual é antes, depois ou igual à data resultante, Dizer a minha idade em dias.");
+    }
+}
