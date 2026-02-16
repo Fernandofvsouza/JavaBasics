5345a57e90de7f8f3216e37513380cc9d5b736b3
Fernando souza <fernandofvsouza@icloud.com>
2026-02-16 16:26:07 +0000

    up

---
+++ JavaLoops/WhileTeorico/While.java
@@ -0,0 +1,18 @@
+/*
+*Como argumento o while so aceita um valoor booleano
+* O loop será executado até que o valor em parenteses seja falso
+*
+*
+* */
+
+
+public class While {
+    static void main() {
+        int n1 = 5;
+        int n2 = 0;
+        while (n1 > n2){
+            System.out.println("Teste: " + n2);
+            n2++;
+        }
+    }
+}
