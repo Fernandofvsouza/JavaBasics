29f93db1bc32581ca686f4383609e268a2cc7bd5
Fernando souza <fernandofvsouza@icloud.com>
2026-02-19 23:44:55 +0000

    LoopFor

---
+++ JavaLoops/ForTeorico/ForTeorico.java
@@ -0,0 +1,20 @@
+/*
+*O for é a estrutura de repetição mais utilizada, pois é a mais simples de ser implementada.
+* Ele é utilizado quando sabemos o número de vezes que queremos repetir um bloco de código.
+*
+* A sintaxe do for é a seguinte:
+* for (inicialização; condição; incremento) {
+*     // bloco de código a ser repetido
+* }
+* */
+
+public class ForTeorico {
+    static void main(String[] args) {
+        // Exemplo de uso do for para imprimir os números de 1 a 10
+       for (int i = 1; i <= 10; i++){
+           System.out.println(i);
+       }
+       //Geralmente damos o nome de i para a variável de controle do for, mas isso é apenas uma convenção, podemos usar qualquer nome.
+
+    }
+}
