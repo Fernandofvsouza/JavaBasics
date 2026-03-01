f87384e6aae98d9cfec809eca8f8a2c1c63a14da
Fernando souza <fernandofvsouza@icloud.com>
2026-03-01 12:45:39 +0000

    Visibiliade

--- Orientação a objetos/POO-TEORIA/2-VisibilidadeEmUmObjeto/VisibilidadeTeoria.java
+++ Orientação a objetos/POO-TEORIA/2-VisibilidadeEmUmObjeto/VisibilidadeTeoria.java
@@ -1,5 +1,13 @@
 public class VisibilidadeTeoria {
     static void main(String[] args) {
-        
+        /*
+        * Modificadores de acesso ou visibilidade:
+        * Indicam o nivel de acesso aos componentes internos(Atributos e metodos) de uma classe
+        * Tipos de modificadores de acesso:
+        * + Public: Acesso permitido a todos os objetos e classes
+        * - Private: Acesso permitido apenas dentro da propria classe
+        * # Protected: Acesso permitido dentro da propria classe e suas subclasses
+        * ~ Default: Acesso permitido apenas dentro do mesmo pacote
+        * */
     }
 }
