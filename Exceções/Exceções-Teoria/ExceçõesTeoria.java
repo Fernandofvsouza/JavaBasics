ee0fbce8f3c2b700c867017d94c929f665944ed8
Fernando souza <fernandofvsouza@icloud.com>
2026-03-09 14:18:09 +0000

    Exceções

---
+++ Exceções/Exceções-Teoria/ExceçõesTeoria.java
@@ -0,0 +1,21 @@
+/*
+* Fluxos Excepcionais
+* Exceção é um fluxo inesperado na aplicação e um erro é a interrupçao do seu sistema
+* Isso significa que uma exceção poderá ser tratada atraves de um desvio de fluxo,
+* enquanto um erro ocasiona na reinicialização do programa
+*
+* Uma das habilidades cruciais de um programador é a previsibilidade de possiveis exceções
+* Exemplos:
+* Tentar usar algum metodo em uma variavel sem referencia (NullPointerException)
+* Tentar ler um arquivo que não existe (FileNotFoundException)
+* Tentar transformar um texto em numero (NumberFormatException)
+* Tentar pegar um elemento que nao existe no array (ArrayIndexOutOfBoundsException)
+*
+*
+* */
+
+
+
+public class ExceçõesTeoria {
+
+}
