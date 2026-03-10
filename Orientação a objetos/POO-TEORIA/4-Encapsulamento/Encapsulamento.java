2ec01c6def06e5c344f2366104d7a62e33fd6d74
Fernando souza <fernandofvsouza@icloud.com>
2026-03-10 13:05:50 +0000

    Encapsulamento

---
+++ Orientação a objetos/POO-TEORIA/4-Encapsulamento/Encapsulamento.java
@@ -0,0 +1,14 @@
+public class Encapsulamento {
+    /*Encapsulamento é um dos pilares da POO, que consiste em
+    * ocultar os detalhes internos de um objeto e fornecer uma interface pública para interagir com ele.
+    *  Isso é feito para proteger os dados e garantir que eles sejam acessados e modificados de maneira controlada.
+    * O encapsulamento é alcançado por meio do uso de modificadores de acesso, como private, protected e public, que controlam a visibilidade dos membros de uma classe.
+    *    * Vantagens do Encapsulamento:
+    * - Tornar mudanças invisíveis para o usuário: O encapsulamento permite que os detalhes internos de uma classe sejam ocultados, o que significa que as mudanças internas não afetam o código que usa a classe.
+    * - Facilitar a reutilização de código: O encapsulamento permite que as classes sejam reutilizadas sem a necessidade de conhecer os detalhes internos, o que torna o código mais modular e fácil de manter.
+    * - Reduzir efeitos colaterais: O encapsulamento ajuda a evitar que mudanças em uma parte do código afetem outras partes, o que torna o código mais robusto e menos propenso a erros.
+    *
+    *   * O encapsulamento não é obrigatorio, mas é uma pratica recomendada para garantir a integridade dos dados e a modularidade do código. Ele é fundamental para a construção de sistemas robustos e fáceis de manter, pois permite que as classes sejam projetadas de forma independente, com uma interface clara e bem definida.
+    *
+    * */
+}
