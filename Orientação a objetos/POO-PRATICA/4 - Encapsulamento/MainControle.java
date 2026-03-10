acb99065239b88e83bbac0f8c2fc230f4b62747b
Fernando souza <fernandofvsouza@icloud.com>
2026-03-10 13:44:01 +0000

    Estudo sobre encapsulamento

---
+++ Orientação a objetos/POO-PRATICA/4 - Encapsulamento/MainControle.java
@@ -0,0 +1,8 @@
+public class MainControle {
+    static void main(String[] args) {
+        ControleRemoto c = new ControleRemoto();
+        c.ligar();
+        c.maisVolume();
+        c.abrirMenu();
+    }
+}
